b23b4c77518584f562ef1b01e52a1b41e8dce8f5
Jonatino <jonathanbeaudoin1996@gmail.com>
2025-02-26 04:20:19 -0500

    fix wasm build

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,11 @@
 #[derive(Debug)]
 pub enum Error {
+    #[cfg(not(target_arch = "wasm32"))]
     TungsteniteError(tungstenite::error::Error),
     IOError(std::io::Error),
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 impl From<tungstenite::error::Error> for Error {
     fn from(error: tungstenite::error::Error) -> Error {
         Error::TungsteniteError(error)
@@ -20,6 +22,7 @@ impl From<Error> for std::io::Error {
     fn from(error: Error) -> std::io::Error {
         match error {
             Error::IOError(error) => error,
+            #[cfg(not(target_arch = "wasm32"))]
             Error::TungsteniteError(e) => std::io::Error::new(std::io::ErrorKind::Other, e),
         }
     }

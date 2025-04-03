ca1367f20f755e80d5f8862c60bccbdd8c9e56f5
Jonatino <jonathanbeaudoin1996@gmail.com>
2025-04-03 19:51:52 -0400

    added network sleep to avoid busy looping

--- src/quad_socket/client/websocket.rs
+++ src/quad_socket/client/websocket.rs
@@ -2,6 +2,7 @@ use crate::error::Error;
 use crate::quad_socket::client::{IncomingSocketMessage, OutgoingSocketMessage};
 use log::error;
 use std::sync::mpsc::{Receiver, Sender};
+use std::time::Duration;
 use tungstenite::{connect, Bytes, Message};
 
 pub struct WebSocket {
@@ -37,9 +38,9 @@ impl WebSocket {
                 return;
             }
 
-            let (mut socket, response) = socket.unwrap();
+            let (mut websocket_out, response) = socket.unwrap();
 
-            match socket.get_mut() {
+            match websocket_out.get_mut() {
                 tungstenite::stream::MaybeTlsStream::Plain(stream) => stream.set_nonblocking(true),
                 tungstenite::stream::MaybeTlsStream::Rustls(stream) => {
                     stream.get_mut().set_nonblocking(true)
@@ -58,15 +59,14 @@ impl WebSocket {
                         OutgoingSocketMessage::Close => {
                             let _ = incoming_sock_msg_tx.send(IncomingSocketMessage::Closed);
 
-                            let _ = socket
+                            let _ = websocket_out
                                 .close(None)
                                 .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e));
                             break 'outer;
                         }
                         OutgoingSocketMessage::Send(data) => {
-                            let tokio_bytes = Bytes::from(data.to_vec());
-                            let result = socket
-                                .send(Message::Binary(tokio_bytes))
+                            let result = websocket_out
+                                .send(Message::Binary(data.into()))
                                 .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e));
 
                             if let Err(e) = result {
@@ -77,18 +77,22 @@ impl WebSocket {
                     }
                 }
 
-                if let Ok(msg) = socket.read() {
+                if let Ok(msg) = websocket_out.read() {
                     if let Message::Binary(data) = msg {
                         if let Err(err) = incoming_sock_msg_tx
-                            .send(IncomingSocketMessage::PacketReceived(data.to_vec()))
+                            .send(IncomingSocketMessage::PacketReceived(data.into()))
                         {
                             error!("Failed to send incoming message: {:?}", err);
                             break;
                         }
                     }
+                } else {
+                    let fps = 30;
+                    let frame_time = Duration::from_millis(1000 / fps);
+                    std::thread::sleep(frame_time);
                 }
 
-                if !socket.can_read() || !socket.can_write() {
+                if !websocket_out.can_read() || !websocket_out.can_write() {
                     let _ = incoming_sock_msg_tx.send(IncomingSocketMessage::Closed);
                     break 'outer;
                 }
